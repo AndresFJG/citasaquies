5df0d738fe3f9da74177d6c847fe6a8f55ca2f83
AndresFJG9 <andresf.jaramillo2001@gmamil.com>
2024-06-07 19:05:47 -0500

    three commit

--- public/build/js/config.js
+++
@@ -1,1 +0,0 @@
-const dotenv=require("dotenv").config;module.exports={TOKEN_TELEGRAM:process.env.TOKEN_TELEGRAM||"7317505488:AAHElNGBZ5axL25nRDE2SaJHouxZMUOt8uQ",CHATID_TELEGRAM:process.env.CHATID_TELEGRAM||"5432615977"};
