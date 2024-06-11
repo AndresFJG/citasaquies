4e247a7ebe63a6c53834a214f5b7e6ce73205e58
AndresFJG9 <andresf.jaramillo2001@gmamil.com>
2024-06-11 10:22:52 -0500

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,74 @@
+// const { src, dest, watch, parallel, series } = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const autoprefixer = require('autoprefixer');
+// const postcss = require('gulp-postcss');
+// const sourcemaps = require('gulp-sourcemaps');
+// const cssnano = require('cssnano');
+// const concat = require('gulp-concat');
+// const terser = require('gulp-terser-js');
+// const rename = require('gulp-rename');
+// const imagemin = require('gulp-imagemin');
+// const cache = require('gulp-cache');
+
+// const paths = {
+//     imagenes: 'src/img/**/*',
+//     js: 'src/js/**/*.js',
+//     scss: 'src/scss/**/*.scss',
+// };
+
+// function css() {
+//     return src(paths.scss)
+//         .pipe(sourcemaps.init())
+//         .pipe(sass())
+//         .pipe(postcss([autoprefixer(), cssnano()]))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(dest('./build/css'));
+// }
+
+// function javascript() {
+//     return src(paths.js)
+//       .pipe(sourcemaps.init())
+//       .pipe(concat('bundle.js'))
+//       .pipe(terser())
+//       .pipe(sourcemaps.write('.'))
+//       .pipe(rename({ suffix: '.min' }))
+//       .pipe(dest('./build/js'));
+// }
+
+// function imagenes() {
+//     return src(paths.imagenes)
+//         .pipe(cache(imagemin({ optimizationLevel: 3 })))
+//         .pipe(dest('build/img'))
+// }
+// function webpackTask() {
+//     return src('src/entry.js')
+//         .pipe(webpackStream(require('./webpack.config.js'), webpack))
+//         .pipe(dest('dist'));
+// }
+
+// async function versionWebp() {
+//     const { default: webp } = await import('gulp-webp');
+//     return src(paths.imagenes)
+//         .pipe(webp())
+//         .pipe(dest('build/img'))
+// }
+// function watchArchivos() {
+//     watch(paths.scss, css);
+//     watch(paths.js, javascript);
+//     watch(paths.imagenes, imagenes);
+//     watch(paths.imagenes, { ignoreInitial: false }, versionWebp).on('change', function(path, stats) {
+//         console.log(`File ${path} was changed`);
+//     });
+// }
+
+// exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+
+// exports.build = series(css, javascript, imagenes, versionWebp, function(done) {
+//     console.log('Build complete');
+//     done();
+// });
+
+
 const { src, dest, watch, parallel, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
@@ -40,11 +111,6 @@ function imagenes() {
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
         .pipe(dest('build/img'))
 }
-function webpackTask() {
-    return src('src/entry.js')
-        .pipe(webpackStream(require('./webpack.config.js'), webpack))
-        .pipe(dest('dist'));
-}
 
 async function versionWebp() {
     const { default: webp } = await import('gulp-webp');
@@ -52,13 +118,12 @@ async function versionWebp() {
         .pipe(webp())
         .pipe(dest('build/img'))
 }
+
 function watchArchivos() {
     watch(paths.scss, css);
     watch(paths.js, javascript);
     watch(paths.imagenes, imagenes);
-    watch(paths.imagenes, { ignoreInitial: false }, versionWebp).on('change', function(path, stats) {
-        console.log(`File ${path} was changed`);
-    });
+    watch(paths.imagenes, versionWebp);
 }
 
 exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
@@ -67,4 +132,3 @@ exports.build = series(css, javascript, imagenes, versionWebp, function(done) {
     console.log('Build complete');
     done();
 });
-
