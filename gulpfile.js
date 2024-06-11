ef4cff0d830ab332bfe784b5d4b45912bfc11792
AndresFJG9 <andresf.jaramillo2001@gmamil.com>
2024-06-11 10:29:16 -0500

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -98,25 +98,25 @@ function css() {
 
 function javascript() {
     return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'));
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./build/js'));
 }
 
 function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('build/img'))
+        .pipe(dest('build/img'));
 }
 
 async function versionWebp() {
     const { default: webp } = await import('gulp-webp');
     return src(paths.imagenes)
         .pipe(webp())
-        .pipe(dest('build/img'))
+        .pipe(dest('build/img'));
 }
 
 function watchArchivos() {
@@ -126,7 +126,7 @@ function watchArchivos() {
     watch(paths.imagenes, versionWebp);
 }
 
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);
 
 exports.build = series(css, javascript, imagenes, versionWebp, function(done) {
     console.log('Build complete');
